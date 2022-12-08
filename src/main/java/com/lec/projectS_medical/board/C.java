7ab902ebf82c65a9f3e4d8f700b15276da97f8a7
HyeJung Kang <116064204+KHJmarina@users.noreply.github.com>
2022-12-08 10:57:47 +0900

    Add files via upload

---
+++ src/main/java/com/lec/projectS_medical/board/C.java
@@ -0,0 +1,9 @@
+package com.lec.projectS_medical.board;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class C {
+	// MyBatis 용 SqlSession
+	public static SqlSession sqlSession;
+}
